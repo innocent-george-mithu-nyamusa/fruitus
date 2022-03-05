f782171711a8f92404d4dedccb5d9a5aa8a1beb9
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2022-03-05 17:19:55 +0200

    Initial Commit

---
+++ Fruitus/FruitusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitusApp.swift
+//  Fruitus
+//
+//  Created by Mithu's Macbook on 5/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
